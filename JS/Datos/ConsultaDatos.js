49e86725e289a6c52e1fdd4fc7a6b3c2b288a71a
reyZele987 <zeledonreymundo@gmail.com>
2025-02-05 19:14:35 -0600

    entregable 1 proyecto

--- JS/Datos/ConsultaDatos.js
+++ JS/Datos/ConsultaDatos.js
@@ -24,13 +24,13 @@ export let Datos_LiNav = [
         Nombre: "Listado Clientes", 
         Detalle: "Practica",     
         Url: "/Practica_Listado_Clientes.html",
-        Activo: false
+        Activo: true
     },
     {   Id: 4,
         Nombre: "Tarea Pokemon",   
         Detalle: "Practica",  
         Url: "/Practica_TareaPokemon.html",
-        Activo: false
+        Activo: true
     },
     {   Id: 5,
         Nombre: "Tarea Netflix",    
@@ -66,7 +66,7 @@ export let Datos_LiNav = [
         Nombre: "Practica IF",    
         Detalle: "Practica ", 
         Url: "/Practica_IF.html",
-        Activo: false
+        Activo: true
     },
     {   Id: 12,
         Nombre: "Practica CICLO",    
